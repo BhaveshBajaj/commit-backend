e36503bb525033ca52f6f287a257056b42868f09
Bhavesh Bajaj <bhavesh.pdt@wheelocity.com>
2026-02-03 20:49:00 +0530

    cors++

--- src/main/java/com/commit/commit/config/WebConfig.java
+++ src/main/java/com/commit/commit/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.commit.commit.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -7,9 +9,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
